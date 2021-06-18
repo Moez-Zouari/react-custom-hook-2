c591aef57567b09c5fcfe9c4d599bd1664741c78
Moez <moez_zouari@hotmail.fr>
2021-06-18 12:15:27 +0100

    Using The  Custom Http Hook

--- src/App.js
+++ src/App.js
@@ -2,8 +2,32 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
+  const [tasks, setTasks] = useState({});
+
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://react-http-1e59e-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
+
   useEffect(() => {
     fetchTasks();
   }, []);
